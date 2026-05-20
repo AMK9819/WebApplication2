524ab742435692950234ebf43a8a218b5153728c
abdal <abdal@DESKTOP-NBGN7M7>
2026-05-20 14:17:31 -0400

    Testiing Web Job Deployment

--- PracticeWebJob/Functions.cs
+++ PracticeWebJob/Functions.cs
@@ -9,7 +9,7 @@ namespace PracticeWebJob
         // on an Azure Queue called queue.
         public static void ProcessQueueMessage([QueueTrigger("testq")] string message, ILogger logger)
         {
-            logger.LogInformation($"Processed queue message 5/19/2026 5:04PM: {message}");
+            logger.LogInformation($"Processed queue message 5/20/2026 2:15PM: {message}");
         }
     }
 }
