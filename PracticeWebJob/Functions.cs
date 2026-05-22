50f2f5163362ced96d64fd824921c00ff70f6311
abdal <abdal@DESKTOP-NBGN7M7>
2026-05-22 15:41:59 -0400

    added mesg to index and edited time for testq mesg

--- PracticeWebJob/Functions.cs
+++ PracticeWebJob/Functions.cs
@@ -9,7 +9,7 @@ namespace PracticeWebJob
         // on an Azure Queue called queue.
         public static void ProcessQueueMessage([QueueTrigger("testq")] string message, ILogger logger)
         {
-            logger.LogInformation($"Processed queue message 5/22/2026 2:48PM: {message}");
+            logger.LogInformation($"Processed queue message 5/22/2026 3:41PM: {message}");
         }
     }
 }
