b0d74f6f2fa43bb58a692f922bfa429075a5bef8
abdal <abdal@DESKTOP-NBGN7M7>
2026-06-01 14:07:01 -0400

    changed time

--- PracticeWebJob/Functions.cs
+++ PracticeWebJob/Functions.cs
@@ -9,7 +9,7 @@ namespace PracticeWebJob
         // on an Azure Queue called queue.
         public static void ProcessQueueMessage([QueueTrigger("testq")] string message, ILogger logger)
         {
-            logger.LogInformation($"Processed queue message 6/1/2026 1:58PM: {message}");
+            logger.LogInformation($"Processed queue message 6/1/2026 2:06PM: {message}");
         }
     }
 }
